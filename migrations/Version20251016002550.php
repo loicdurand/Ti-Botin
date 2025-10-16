1d59412d3b55f3b763891676e557d365691af9e7
Loïc DURAND <loicdurand.ld@gmail.com>
2025-10-16 01:03:40 +0000

    sync db sur pc perso

---
+++ migrations/Version20251016002550.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20251016002550 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user CHANGE mail mail VARCHAR(100) DEFAULT NULL, CHANGE civilite civilite VARCHAR(3) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE `user` CHANGE mail mail VARCHAR(255) DEFAULT NULL, CHANGE civilite civilite VARCHAR(8) DEFAULT NULL');
+    }
+}
